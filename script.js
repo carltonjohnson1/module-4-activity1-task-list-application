833896e1d32da3e7c1f67d65db009761cadc5f76
carltonjohnson1 <carltonj1@hotmail.com>
2025-08-14 23:04:02 +0100

    final version with updated javascript

--- script.js
+++ script.js
@@ -3,44 +3,53 @@
 let tasks = [];
 
 
+// Event listener for adding new task by running the function add Task on click
+document.querySelector("#newTask").addEventListener("click", function (e) {
+    addTask();
+});
+
+// function for adding a task
+
 function addTask () {
-    const taskInput = document.querySelector("#taskInput");
-    const text = taskInput.value.trim();
+    const taskInput = document.querySelector("#taskInput"); // getting the task input field
+    const text = taskInput.value.trim(); // storing the value of taskinput in the text variable and removing any white space either side of the input
 
     if (text) {
-        tasks.push({ text: text, completed: false });
-        taskInput.value = "";
-        updateTasksList();
+        tasks.push({ text: text, completed: false }); // adding a new task and checking it's not already complete
+        taskInput.value = "";// clearing the input field so the user can type in it again
+        updateTasksList(); //calling the update Taskslist function to update the list with the new item
     }
 };
 
+// Marking a task as complete or incomplete and updating the list 
 function toggleTaskComplete (index) {
     tasks[index].completed = !tasks[index].completed;
     updateTasksList();
-
 };
 
+
+// deleting a task from the list and updating the list
 function deleteTask (index) {
     tasks.splice(index,1);
     updateTasksList();
  
 };
 
+//allowing for editing of a task and then updatin the list
 function editTask (index) {
     taskInput = document.querySelector("#taskInput");
     taskInput.value = tasks[index].text;
-
     tasks.splice(index,1);
     updateTasksList();
-
 };
 
 
-function updateTasksList () {
+// displaying the task list correctly by rebuilding it properly when function is called
+function updateTasksList() {
     const taskList = document.querySelector("#task-list");
     taskList.innerHTML = "";
 
-    tasks.forEach((task, index) => {
+    tasks.forEach(function(task, index) {
         const listItem = document.createElement("li");
 
         listItem.innerHTML = `
@@ -60,15 +69,12 @@ function updateTasksList () {
 
         // Add checkbox event
         const checkbox = listItem.querySelector(".checkbox");
-        checkbox.addEventListener("change", () => toggleTaskComplete(index));
+        checkbox.addEventListener("change", function() {
+            toggleTaskComplete(index);
+        });
 
         taskList.appendChild(listItem);
     });
-};
+}
 
 
-// Submit event
-document.querySelector("#newTask").addEventListener("click", function (e) {
-    e.preventDefault();
-    addTask();
-});
